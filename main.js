10e8b32a2de265211e1645cdb7d196d2e4a1403d
Tatiana Anderson <happytasha9933@gmail.com>
2025-10-14 11:34:45 +0400

    work on sections why Georgia and wjat we provide

---
+++ main.js
@@ -0,0 +1,42 @@
+'use strict'
+
+
+
+
+// const btn1 = document.getElementById('btn-1');
+
+
+// let promise = new Promise((resolve, reject) => {
+//     console.log('resolved');
+
+//     let count = 10;
+//     btn1.addEventListener('click', function(){
+//         resolve(count);
+//         clearInterval(timer);
+//     })
+
+//     let timer = setInterval(function () {
+//         count--;
+//         btn1.textContent = 'Apply Now ' + count;
+
+//         if (count == 0){
+//             reject();
+//             clearInterval(timer);
+//         }
+//     }, 500);
+        
+// });
+
+
+// promise.then(
+//     function (count){
+//         btn1.style.background = 'green';
+//         console.log('Assingment is finished on the ' +count+  ' second');
+//     }, 
+//     function(){
+//         console.log('rejected');
+//         btn1.style.background = 'red';
+//     }
+// );
+
+
