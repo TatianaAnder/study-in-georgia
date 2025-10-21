837151026fa5511859c4379036a0e4d2bfede74d
Tatiana Anderson <happytasha9933@gmail.com>
2025-10-21 22:02:01 +0400

    add header and footer and about pages

---
+++ include.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('pages/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+
+    fetch('pages/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+})
